81cf1522d080c979097ada85b33cd6b76d16c037
Okorieuchebg <uche.okorie@babbangona.com>
2023-03-22 18:24:01 +0100

    updated the NoteAdapter housing the view holder and binders, i applied the diffUtil from the binding library

--- app/src/main/java/com/udemylearn/noteappproject/adapter/NoteAdapter.kt
+++ app/src/main/java/com/udemylearn/noteappproject/adapter/NoteAdapter.kt
@@ -1,38 +1,59 @@
 package com.udemylearn.noteappproject.adapter
 
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.udemylearn.noteappproject.databinding.NoteViewLayoutBinding
+import com.udemylearn.noteappproject.fragments.NoteHomeFragmentDirections
 import com.udemylearn.noteappproject.model.Note
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
-    inner class NoteViewHolder(itemBinding: NoteViewLayoutBinding) :
-        RecyclerView.ViewHolder(itemBinding.root){
-
-        }
+    inner class NoteViewHolder(var itemBinding: NoteViewLayoutBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
 
     private val differCallback = object : DiffUtil.ItemCallback<Note>(){
         override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
-            TODO("Not yet implemented")
+            return oldItem.id == newItem.id &&
+                    oldItem.noteBody == newItem.noteBody &&
+                    oldItem.noteTitle == newItem.noteTitle
         }
 
         override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
-            TODO("Not yet implemented")
+            return oldItem == newItem
         }
     }
 
+    private val differ = AsyncListDiffer(this, differCallback)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        TODO("Not yet implemented")
+        return NoteViewHolder(
+            NoteViewLayoutBinding.inflate(LayoutInflater.from(parent.context),
+                parent,
+                false)
+        )
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        var currentList = differ.currentList[position]
+
+        holder.itemBinding.tvLayoutNoteBody.text =
+            currentList.noteBody
+
+        holder.itemBinding.tvLayoutNoteTitle.text =
+            currentList.noteTitle
+
+        holder.itemView.setOnClickListener{
+            it.findNavController().navigate(NoteHomeFragmentDirections.actionNoteHomeFragmentToUpdateNoteFragment(currentList))
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return differ.currentList.size
     }
 }
